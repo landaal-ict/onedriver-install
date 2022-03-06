04466b3c1e971e3e7abec65e76786cfad6f12531
landaalict <wesley@familielandaal.nl>
2022-03-06 22:45:31 +0100

    update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # onedriver-install
  
+Onedriver install script supports:
+
+Ubuntu Ubuntu 20.04.4 LTS
---
+++ onedriver.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo 'deb http://download.opensuse.org/repositories/home:/jstaf/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/home:jstaf.list
+curl -fsSL https://download.opensuse.org/repositories/home:jstaf/xUbuntu_20.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_jstaf.gpg > /dev/null
+sudo apt update
+sudo apt install onedriver
